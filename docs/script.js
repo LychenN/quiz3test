7aac67c460ef7f3fb122a19abe2c410dba84ac55
Yuanchen Li <lyc1070819770@163.com>
2022-03-26 02:23:02 -0400

    3

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ window.onload = () => {
      renderPlaces(places);// This two line will load the fuction renderplacese(places)
 };
 
-function staticLoadPlaces() {//when load place first load staticloadplaces location lat and lng,then loadthe renderplaces and shows the detail of the latitude and the longitude
+function staticLoadPlaces() { //when load place first load staticloadplaces location lat and lng,then loadthe renderplaces and shows the detail of the latitude and the longitude
     return [
         {
             name: 'MyModel',
@@ -25,7 +25,7 @@ function renderPlaces(places) {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);//model's location detail
         model.setAttribute('gltf-model', './assets/MyModel/scene.gltf');//locate and pu a model to the place
-        model.setAttribute('rotation', '0 180 0');//can rotate the camera to find the model
+        model.setAttribute('rotation', '0 90 0');//can rotate the camera to find the model
         model.setAttribute('animation-mixer', '');//mix the gps and the model in the camera
         model.setAttribute('scale', '0.5 0.5 0.5');//to change the model size
 
